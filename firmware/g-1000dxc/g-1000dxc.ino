b3e9246697068fd260f658c841efaf213a23859f
Jan Janak <jan@janakj.org>
2022-08-14 17:52:58 +0200

    Various fixes and improvements

--- firmware/g-1000dxc/g-1000dxc.ino
+++ firmware/g-1000dxc/g-1000dxc.ino
@@ -20,6 +20,28 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+// The following Python program was used to fit a Polynomial to the measured
+// values using the least squares method. The coefficients and the domain of the
+// generated polynomial must be passed as arguments to the PolynomialInput
+// constructors used in this source file.
+//
+//     import numpy as np
+//     import matplotlib.pyplot as plt
+//     import scipy.stats as stats
+//     from numpy.polynomial import Polynomial
+//
+//     x = [...]
+//     y = [...]
+//
+//     curve = Polynomial.fit(x, y, 8)
+//     print(repr(curve))
+//     print(curve)
+//
+//     yf = [curve(x) for x in x]
+//
+//     plt.plot(x, y)
+//     plt.plot(x, yf)
+//     plt.show()
 
 // TODO
 // * Automatically scale PWM output values depending on the PWM resolution
@@ -31,9 +53,9 @@
 
 // ======== The following values may need to be configured ========
 
-#define WIFI_SSID "<wifi ssid>"
-#define WIFI_PASSWORD "<wifi password>"
-#define MDNS_NAME "<zeroconf name>"
+//#define WIFI_SSID     "Queeg"
+//#define WIFI_PASSWORD "spravedlnosti14"
+//#define MDNS_NAME     "rotator"
 
 // The size of the overlap region in degrees
 #define OVERLAP 90
@@ -89,9 +111,9 @@
 #include <ESPmDNS.h>
 #include <ESPAsyncWebServer.h>
 
-#define MAX_AZIMUTH (360 + OVERLAP)
+#define MAX_ORIENTATION (360 + OVERLAP)
 
-#define NO_AZIMUTH -1
+#define NO_ORIENTATION -1
 
 using namespace std;
 
@@ -111,7 +133,7 @@ typedef struct inputs {
     int16_t speed;
     int16_t rotator;
     int16_t indicator;
-    int16_t azimuth;
+    int16_t orientation;
 } inputs_t;
 
 
@@ -316,6 +338,9 @@ public:
 
 
 class Adc: public AnalogInput {
+private:
+    uint16_t current;
+
 protected:
     uint8_t gpio;
     int oversample;
@@ -343,36 +368,74 @@ public:
 
         history.push_back(analogRead(gpio));
         sum += history.back();
-        return sum / oversample;
+        current = sum / oversample;
+        return current;
     }
 
     virtual int get_raw() override {
-        return analogRead(gpio);
+        return current;
     }
 };
 
 
-class LinearInput: public AnalogInput {
+class PolynomialFit: public AnalogInput {
 private:
     AnalogInput &input;
-    const array<int,2> p1;
-    const array<int,2> p2;
-    int x;
+    float offset;
+    float scale;
+    const vector<float> coefficients;
 
 public:
-    LinearInput(AnalogInput &input, const array<int,2> &p1, const array<int, 2> &p2, int min=INT_MIN, int max=INT_MAX):
-        AnalogInput(min, max), input(input), p1(p1), p2(p2) { }
+    PolynomialFit(AnalogInput &input, const array<int, 2> &domain, const vector<float> &coefficients):
+        input(input), coefficients(coefficients) {
+        const auto window = array<int, 2>{-1, 1};
+
+        // TODO: Initialize AnalogInput with minimum / maximum computed from the domain.
+
+        // Compute the parameters for a linear translation of input values from
+        // <domain> to a window of <-1, 1>. The coefficients passed in the
+        // parameter coefficient must have been fitted for the window, not for
+        // the domain.
+        float dlen = domain[1] - domain[0];
+        float wlen = window[1] - window[0];
+        offset = ((float)domain[1] * (float)window[0] - (float)domain[0] * (float)window[1]) / dlen;
+        scale = wlen / dlen;
+    }
 
     virtual int get() override {
-        x = input.get();
-        int y = (p1[1] * (p2[0] - x) + p2[1] * (x - p1[0])) / (p2[0] - p1[0]);
-        if (y < min) y = min;
-        if (y > max) y = max;
-        return y;
+        // Linearly translate the input value to the window.
+        float x = input.get() * scale + offset;
+
+        // Compute the value using Horner's method.
+        double sum = coefficients.back();
+        for (int i = coefficients.size() - 2; i >= 0; i--)
+            sum = coefficients[i] + sum * x;
+
+        return (int)round(sum);
     }
 
     virtual int get_raw() override {
+        return input.get_raw();
+    }
+};
+
+
+class Clamped: public AnalogInput {
+private:
+    AnalogInput &input;
+
+public:
+    Clamped(AnalogInput &input, int min, int max): AnalogInput(min, max), input(input) {}
+
+    virtual int get() override {
+        int x = input.get();
+        if (x < min) return min;
+        if (x > max) return max;
         return x;
+    }
+
+    virtual int get_raw() override {
+        return input.get_raw();
     }
 };
 
@@ -386,8 +449,8 @@ public:
     const int min;
     const int max;
 
-    Rotator(VariableSpeedMotor &motor, AnalogInput &sensor):
-        motor(motor), sensor(sensor), min(0), max(MAX_AZIMUTH) {
+    Rotator(VariableSpeedMotor &motor, AnalogInput &sensor, int min = INT_MIN, int max = INT_MAX):
+        motor(motor), sensor(sensor), min(min), max(max) {
     }
 
     void stop(void) {
@@ -395,12 +458,12 @@ public:
     }
 
     void cw() {
-        if (sensor.get() < sensor.max) motor.cw();
+        if (sensor.get() < max) motor.cw();
         else stop();
     }
 
     void ccw() {
-        if (sensor.get() > sensor.min) motor.ccw();
+        if (sensor.get() > min) motor.ccw();
         else stop();
     }
 
@@ -431,11 +494,11 @@ static Rotator *rotator, *indicator;
 static AnalogInput *preset;
 static AnalogInput *speed;
 
-
 static inputs_t inputs, previous_inputs;
 
 
-void init_wifi()
+#if defined(WIFI_SSID) && defined(WIFI_PASSWORD)
+static void init_wifi()
 {
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -451,9 +514,11 @@ void init_wifi()
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 }
+#endif
 
 
-void init_mdns()
+#if defined(MDNS_NAME)
+static void init_mdns()
 {
     Serial.print("Starting mDNS...");
     if (!MDNS.begin(MDNS_NAME)) {
@@ -463,26 +528,55 @@ void init_mdns()
     Serial.println("done.");
     Serial.print("mDNS name: ");
     Serial.println(MDNS_NAME);
+
+    MDNS.addService("http", "tcp", 80);
+}
+#endif
+
+
+static uint16_t orientation_to_azimuth(uint16_t v)
+{
+    if (v < 180) return v + 180;
+    else if (v >= 180 && v < 360) return v - 180;
+    else return v - 180;
 }
 
 
-String processor(const String& var)
+static uint16_t azimuth_to_orientation(uint16_t v)
 {
-    if (var == "ANTENNA") return String(inputs.rotator);
+    if (v < 180) return v + 180;
+    else if (v >= 180 && v < 360) return v - 180;
+    else return v;
+}
+
+
+static String processor(const String& var)
+{
+    if (var == "ANTENNA") return String(orientation_to_azimuth(inputs.rotator));
     if (var == "ANTENNA_RAW") return String(rotator->get_raw());
-    if (var == "INDICATOR") return String(inputs.indicator);
+
+    if (var == "INDICATOR") return String(orientation_to_azimuth(inputs.indicator));
     if (var == "INDICATOR_RAW") return String(indicator->get_raw());
-    if (var == "PRESET") return String(inputs.preset);
+
+    if (var == "PRESET_KNOB") return String(orientation_to_azimuth(inputs.preset));
+    if (var == "PRESET_KNOB_RAW") return String(preset->get_raw());
+
     if (var == "SPEED") return String(inputs.speed);
-    if (var == "AZIMUTH") return String(inputs.azimuth);
+    if (var == "SPEED_RAW") return String(speed->get_raw());
+
+    if (var == "PRESET") {
+        if (inputs.orientation == NO_ORIENTATION) {
+            return String();
+        } else {
+            return String(orientation_to_azimuth(inputs.orientation));
+        }
+    }
     return String();
 }
 
 
-void init_http()
+static void init_http()
 {
-    MDNS.addService("http", "tcp", 80);
-
     static AsyncWebServer web_server(80);
 
     web_server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -493,10 +587,11 @@ void init_http()
         request->send(SPIFFS, "/style.css", "text/css");
     });
 
-    web_server.on("/set", HTTP_GET, [](AsyncWebServerRequest * request) {
+    web_server.on("/set-azimuth", HTTP_GET, [](AsyncWebServerRequest * request) {
         if (request->hasParam("azimuth")) {
             String v = request->getParam("azimuth")->value();
-            inputs.azimuth = stoi(v.c_str());
+            v.trim();
+            inputs.orientation = v.length() == 0 ? NO_ORIENTATION : azimuth_to_orientation(stoi(v.c_str()));
         }
         request->redirect("/");
     });
@@ -532,11 +627,15 @@ void setup()
     }
 
     memset(&inputs, '\0', sizeof(inputs));
-    inputs.azimuth = NO_AZIMUTH;
+    inputs.orientation = NO_ORIENTATION;
 
-    // init_wifi();
-    // init_mdns();
-    // init_http();
+#if defined(WIFI_SSID) && defined(WIFI_PASSWORD)
+    init_wifi();
+#if defined(MDNS_NAME)
+    init_mdns();
+#endif
+    init_http();
+#endif
 
     static Gpio preset_gpio(PRESET_SWITCH, INPUT_PULLUP);
     static Gpio ccw_gpio(CCW_BUTTON, INPUT_PULLUP);
@@ -567,14 +666,31 @@ void setup()
     // The linear mapping isn't ideal, either the ADC or the potentiometer is
     // slightly non-linear, but it will do.
     static Adc preset_adc(PRESET_SENSOR);
-    static LinearInput preset_(preset_adc, array<int, 2>{302, 0},
-        array<int, 2>{655, MAX_AZIMUTH}, 0, MAX_AZIMUTH);
-    preset = &preset_;
+
+    // The following values were measured on the physical preset button. The
+    // first row contains raw sensor values. The second row contains preset
+    // orientations from 0 to 450.
+    //
+    // 299 | 323 | 347 | 373 | 399 | 425 | 448 | 472 | 495 | 518 | 542 | 564 | 584 | 610 | 631 | 651
+    //   0 |  30 |  60 |  90 | 120 | 150 | 180 | 210 | 240 | 270 | 300 | 330 | 360 | 390 | 420 | 450
+    //
+    // Fitting a polynomial to the measured values produced the following
+    // parameters:
+    static PolynomialFit preset_fitted(preset_adc, array<int, 2>{ 299, 651 }, vector<float>{
+        214.13017251512497, 225.77818898710638, -0.8075325747516042,
+        -9.47189296911141, 98.44743275442832, -1.040438358931594,
+        -185.95051207672086, 9.813429808629284, 99.26608067691565
+    });
+    static Clamped preset_clamped(preset_fitted, 0, MAX_ORIENTATION);
+    preset = &preset_clamped;
 
     // The speed knob is a potentiometer linearly mapped to the <0, 255> range.
-    static Adc speed_adc(SPEED_SENSOR);
-    static LinearInput speed_(speed_adc, array<int, 2>{307, 0}, array<int, 2>{638, 255}, 0, 255);
-    speed = &speed_;
+    static Adc speed_adc(SPEED_SENSOR, 256);
+    static PolynomialFit speed_fitted(speed_adc, array<int, 2>{ 307, 638 }, vector<float>{
+        127.49999999999997, 127.50000000000001
+    });
+    static Clamped speed_clamped(speed_fitted, 0, 255);
+    speed = &speed_clamped;
 
     // The indicator consists of two GPIO output pins connected to the L293D
     // driver chip that rotate the indicator motor clockwise or
@@ -592,9 +708,27 @@ void setup()
     static VariableSpeedMotor indicator_motor(indicator_cw_gpio, indicator_ccw_gpio, indicator_speed);
 
     static Adc indicator_sensor_adc(INDICATOR_SENSOR);
-    static LinearInput indicator_sensor(indicator_sensor_adc, array<int, 2>{322, 0},
-        array<int, 2>{630, MAX_AZIMUTH}, 0, MAX_AZIMUTH);
-    static Rotator indicator_(indicator_motor, indicator_sensor);
+
+    // 322 | 329 | 335 | 343 | 350 | 356 | 364 | 370 | 378 | 386
+    //   0 |  10 |  20 |  30 |  40 |  50 |  60 |  70 |  80 |  90
+
+    // 392 | 400 | 407 | 413 | 420 | 426 | 434 | 440 | 446 | 453
+    // 100 | 110 | 120 | 130 | 140 | 150 | 160 | 170 | 180 | 190
+
+    // 460 | 467 | 473 | 480 | 487 | 493 | 500 | 507 | 515 | 522
+    // 200 | 210 | 220 | 230 | 240 | 250 | 260 | 270 | 280 | 290
+
+    // 528 | 534 | 541 | 549 | 555 | 562 | 569 | 575 | 582 | 589
+    // 300 | 310 | 320 | 330 | 340 | 350 | 360 | 370 | 380 | 390
+
+    // 595 | 602 | 610 | 618 | 624 | 630
+    // 400 | 410 | 420 | 430 | 440 | 450
+    static PolynomialFit indicator_sensor_fitted(indicator_sensor_adc, array<int, 2>{ 322, 630}, vector<float>{
+        224.32983151113456, 227.975502016654, -23.318904870464237,
+        2.928610614473806, 105.82445830796173, -21.214288612988362,
+        -149.52566528385233, 15.029923558472099, 67.52449512618676
+    });
+    static Rotator indicator_(indicator_motor, indicator_sensor_fitted, 0, MAX_ORIENTATION);
     indicator = &indicator_;
 
     // The rotator consists of two GPIO output pins connected to the L293D
@@ -608,14 +742,27 @@ void setup()
     static Dac speed_control(SPEED_CONTROL, DAC_CHANNEL_1);
     static VariableSpeedMotor rotator_motor(motor_cw_gpio, motor_ccw_gpio, speed_control);
     static Adc rotator_sensor_adc(ROTATOR_SENSOR);
-    static LinearInput rotator_sensor(rotator_sensor_adc, array<int, 2>{14, 5},
-        array<int, 2>{901, MAX_AZIMUTH - 5});
-    static Rotator rotator_(rotator_motor, rotator_sensor);
+
+    // The following values were measured on the physical antenna rotator. The
+    // first row contains raw rotator sensor values. The second row contains
+    // antenna's orientations from 0 to 450.
+    //
+    // 20 | 76 | 126 | 176 | 234 | 294 | 356 | 417 | 480 | 532 | 577 | 637 | 698 | 759 | 828 | 900
+    //  0 | 30 |  60 |  90 | 120 | 150 | 180 | 210 | 240 | 270 | 300 | 330 | 360 | 390 | 420 | 450
+    //
+    // A polynomial was fitted using the least squares method to the measured
+    // value. The fit produced the following parameters:
+    static PolynomialFit rotator_sensor_fitted(rotator_sensor_adc, array<int, 2>{ 20, 900 }, vector<float>{
+        231.06693256776882, 236.21224191173644, 73.9423590843075,
+        -50.288648819706715, -238.6488923001425, 89.08520518133807,
+        251.9955496837724, -49.982680307885396, -93.3990214432426
+    });
+    static Rotator rotator_(rotator_motor, rotator_sensor_fitted);
     rotator = &rotator_;
 }
 
 
-void handle_overlap()
+static void handle_overlap()
 {
     // Turn the overlap indicator on if we get within half of the overlap band
     // from either side. The original controller would turn the indicator on
@@ -628,7 +775,7 @@ void handle_overlap()
 }
 
 
-void handle_speed()
+static void handle_speed()
 {
     // This function attempts to match the speed of the rotator and indicator
     // motors over the entire range of the speed knob. The following polynomial
@@ -644,16 +791,21 @@ void handle_speed()
     //
     // with the following program:
     //
-    // import numpy as np import matplotlib.pyplot as plt import scipy.stats as
-    // stats
+    // import numpy as np
+    // import matplotlib.pyplot as plt
+    // import scipy.stats as stats
     //
-    // x = [0, 63, 127, 191, 255] y = [32, 36, 47, 66, 75]
+    // x = [0, 63, 127, 191, 255]
+    // y = [32, 36, 47, 66, 75]
     //
-    // curve = np.poly1d(np.polyfit(x, y, 4)) print(curve)
+    // curve = np.poly1d(np.polyfit(x, y, 4))
+    // print(curve)
     //
     // y2 = [curve(x) for x in x]
     //
-    // plt.plot(x, y) plt.plot(x, y2) plt.show()
+    // plt.plot(x, y)
+    // plt.plot(x, y2)
+    // plt.show()
 
     int x = speed->get();
     rotator->set_speed(x);
@@ -663,51 +815,60 @@ void handle_speed()
 }
 
 
-void handle_preset()
+static void handle_preset_button()
 {
-    // If the preset button was pressed, transfer the preset azimut value from
-    // the input to the internal state which will make the rotator start
-    // orienting the antenna toward that azimuth. The operation can be canceled
-    // by pressing either the cw or ccw button.
+    // If the preset button was pressed, transfer the preset orientation value
+    // from the input to the internal orientation state variable which will make
+    // the rotator start orienting the antenna toward that orientation. The
+    // operation can be canceled by pressing either the cw or ccw button.
 
     bool preset_pressed = !previous_inputs.preset_button && inputs.preset_button;
+    if (preset_pressed) inputs.orientation = inputs.preset;
+}
 
-    if (preset_pressed) {
-        if (inputs.rotator < inputs.preset) {
-            inputs.azimuth = inputs.preset;
+
+static void rotate_to_orientation() {
+    if (inputs.orientation != NO_ORIENTATION) {
+        if (inputs.rotator < inputs.orientation) {
             indicator->cw();
             rotator->cw();
-        } else if (inputs.rotator > inputs.preset) {
-            inputs.azimuth = inputs.preset;
+        } else if (inputs.rotator > inputs.orientation) {
             indicator->ccw();
             rotator->ccw();
         } else {
-            inputs.azimuth = NO_AZIMUTH;
+            inputs.orientation = NO_ORIENTATION;
             indicator->stop();
             rotator->stop();
         }
     }
 
-    // If we are chasing a preset azimuth, check whether we have reached the
-    // target and in that case turn the rotator and indicators off.
-    if (inputs.azimuth != NO_AZIMUTH) {
+    // This can be triggered by the user trying to stop the rotator by
+    // submitting a "NO_ORIENTATION" value through the web interface.
+    if (previous_inputs.orientation != NO_ORIENTATION && inputs.orientation == NO_ORIENTATION) {
+        indicator->stop();
+        rotator->stop();
+    }
+
+    // If we are chasing a preset orientation, check whether we have reached the
+    // target and in that case turn the rotator and indicator off.
+    if (inputs.orientation != NO_ORIENTATION) {
         if (inputs.ccw_button || inputs.cw_button) {
-            inputs.azimuth = NO_AZIMUTH;
+            inputs.orientation = NO_ORIENTATION;
             indicator->stop();
             rotator->stop();
         } else {
             switch(indicator->state()) {
                 case MOVING_CW:
-                    if (inputs.indicator >= inputs.azimuth) {
-                        inputs.azimuth = NO_AZIMUTH;
+                    if (inputs.indicator >= inputs.orientation) {
+                        inputs.orientation = NO_ORIENTATION;
                         indicator->stop();
                         rotator->stop();
                     }
                     break;
 
                 case MOVING_CCW:
-                    if (inputs.indicator <= inputs.azimuth) {
-                        inputs.azimuth = NO_AZIMUTH;
+                    if (inputs.indicator <= inputs.orientation) {
+                        inputs.orientation = NO_ORIENTATION;
                         indicator->stop();
                         rotator->stop();
                     }
@@ -718,11 +879,11 @@ void handle_preset()
 }
 
 
-void handle_buttons()
+static void handle_buttons()
 {
     // Only respond to CW and CCW buttons if we are not rotating to a preset
-    // azimuth.
-    if (inputs.azimuth != NO_AZIMUTH) return;
+    // orientation.
+    if (inputs.orientation != NO_ORIENTATION) return;
 
     if (inputs.cw_button) {
         rotator->cw();
@@ -741,12 +902,12 @@ void handle_buttons()
 }
 
 
-void handle_sync()
+static void handle_sync()
 {
-    // If none of the buttons is pressed and we do not have a preset azimuth,
+    // If none of the buttons is pressed and we do not have a preset orientation,
     // check that the position of the rotator matches the position of the
     // indicator and if not, adjust the indicator.
-    if (!inputs.cw_button && !inputs.ccw_button && inputs.azimuth == NO_AZIMUTH) {
+    if (!inputs.cw_button && !inputs.ccw_button && inputs.orientation == NO_ORIENTATION) {
         if (inputs.indicator < (inputs.rotator - INDICATOR_TOLERANCE)) {
             indicator->cw();
         } else if (inputs.indicator > (inputs.rotator + INDICATOR_TOLERANCE)) {
@@ -758,7 +919,7 @@ void handle_sync()
 }
 
 
-void read_inputs(void)
+static void read_inputs(void)
 {
     inputs.preset_button = preset_button->get();
     inputs.cw_button = cw_button->get();
@@ -780,7 +941,8 @@ void loop()
 
     handle_speed();
     handle_overlap();
-    handle_preset();
+    handle_preset_button();
+    rotate_to_orientation();
     handle_buttons();
     handle_sync();
 
